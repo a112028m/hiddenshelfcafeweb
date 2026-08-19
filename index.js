3f79090144e50f0de0ff82e467108c9fbfed8e97
MeyiMiyuki <14may2028@gmail.com>
2026-08-19 23:01:13 +0800

    livenav
    ww

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const MOB = document.querySelector("#menu-open-button");
 const MCB = document.querySelector("#menu-close-button");
+const nl = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
 
+const opt = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const id = entry.target.getAttribute('id');
+      nl.forEach(link => link.classList.remove('active'));
+      document.querySelector(`.nav-link[href="#${id}"]`).classList.add('active');
+    }
+  });
+}, opt);
+
+sections.forEach(section => observer.observe(section));
 
 MOB.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 })
 
 MCB.addEventListener("click", () => MOB.click());
-
-
 
 const swiper = new Swiper('.slider-wrapper', {
   loop: true,
